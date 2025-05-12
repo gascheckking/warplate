fe3c71089ea75d6ee025166303e70c538b0e0d08
gascheckking <warpai2k@gmail.com>
2025-05-12 22:44:34 +0200

    Update WarpPoints.sol

--- contracts/WarpPoints.sol
+++ contracts/WarpPoints.sol
@@ -2,23 +2,34 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./FrameValidator.sol";
+import "./WarpXP.sol";
 
-contract WarpPoints is ERC20, Ownable {
+contract WarpPoints is ERC20 {
+    FrameValidator private _validator;
+    WarpXP private _warpXP;
+
     mapping(address => uint256) public lastClaim;
     mapping(address => uint256) public streaks;
-    mapping(address => uint256) public xp;
 
-    uint256 public dailyReward = 100 ether;
-    uint256 public xpPerClaim = 10;
+    constructor(
+        address validatorAddress,
+        address warpXPAddress
+    ) ERC20("WarpPoints", "WARP") {
+        _validator = FrameValidator(validatorAddress);
+        _warpXP = WarpXP(warpXPAddress);
+    }
 
-    constructor() ERC20("WarpPoints", "WARP") {}
-
-    function claimDaily(bytes calldata) external {
+    function claimDaily(bytes calldata frameProof) external {
+        require(_validator.validateFrame(msg.sender, bytes32(frameProof)), "Invalid frame");
         require(block.timestamp - lastClaim[msg.sender] >= 1 days, "Wait 24h");
-        streaks[msg.sender] = block.timestamp - lastClaim[msg.sender] <= 2 days ? streaks[msg.sender] + 1 : 1;
-        _mint(msg.sender, dailyReward * streaks[msg.sender]);
-        xp[msg.sender] += xpPerClaim * streaks[msg.sender];
+
+        streaks[msg.sender] = block.timestamp - lastClaim[msg.sender] <= 2 days 
+            ? streaks[msg.sender] + 1 
+            : 1;
+
+        _mint(msg.sender, 100 * 10**18 * streaks[msg.sender]);
+        _warpXP.addXP(msg.sender, 10 * streaks[msg.sender]);
         lastClaim[msg.sender] = block.timestamp;
     }
 }
