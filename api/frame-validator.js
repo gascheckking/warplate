937386320d2ce29457ef1fbdeee76690127fb446
gascheckking <warpai2k@gmail.com>
2025-05-12 22:44:04 +0200

    Update frame-validator.js

--- api/frame-validator.js
+++ api/frame-validator.js
@@ -1,4 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-export default function handler(req, res) {
-  res.status(200).json({ valid: true });
+contract FrameValidator {
+    address private _owner;
+    mapping(bytes32 => bool) private _approvedHashes;
+
+    constructor() {
+        _owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == _owner, "Not authorized");
+        _;
+    }
+
+    function validateFrame(
+        address user, 
+        bytes32 frameHash
+    ) external view returns (bool) {
+        return _approvedHashes[keccak256(abi.encodePacked(user, frameHash))];
+    }
+
+    function approveFrame(
+        address user, 
+        bytes32 frameHash
+    ) external onlyOwner {
+        _approvedHashes[keccak256(abi.encodePacked(user, frameHash))] = true;
+    }
 }
